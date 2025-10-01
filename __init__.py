38e2d3f237639f2e2b7c7d0ad04e32b13bcd1c3e
deliium <notmagil@gmail.com>
2025-10-01 21:02:05 +0300

    remove some files

--- __init__.py
+++
@@ -1,6 +0,0 @@
-"""Mukit Bot - A Telegram bot for managing selected messages with pinned summaries."""
-
-__version__ = "1.0.0"
-__author__ = "Mukit Bot"
-__description__ = "A Telegram bot that automatically processes messages starting with '.' and creates pinned summaries"
-
