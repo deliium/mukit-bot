873618972b7f3c2c3a6c03716ccca5682e490561
deliium <notmagil@gmail.com>
2025-10-01 14:39:23 +0300

    fix venv path

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/env bash
 
-source /home/mukitby/virtualenv/mukit/3.12/bin/activate
-cd /home/mukitby/mukit || exit
+source /home/mukitby/virtualenv/bot.mukit.by/3.12/bin/activate && cd /home/mukitby/bot.mukit.by || exit
 pip install -r requirements.txt
